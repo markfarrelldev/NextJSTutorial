2d462cdf98e796968c5ce353bab9155b7b5dadc8
Felz <mark.farrell.dev@gmail.com>
2026-09-10 16:16:50 -0600

    last bug fixed

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -25,7 +25,7 @@ export default function EditInvoiceForm({
   const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
   return (
-    <form action={formAction} aria-describedby="form-error">
+    <form action={formAction as any} aria-describedby="form-error">
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
